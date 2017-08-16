053cbf0b1206be2c51eb1f7bf698addf36b159b4
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-08-16 16:44:45 +0200

    :white_check_mark: add some basic tests

---
+++ test/index.js
@@ -0,0 +1,109 @@
+var test = require('tape')
+var encdown = require('..')
+var memdown = require('memdown')
+
+test('opens and closes the underlying db', function (t) {
+  var _db = {
+    open: function (opts, cb) {
+      t.pass('open called')
+      setImmediate(cb)
+    },
+    close: function (cb) {
+      t.pass('close called')
+      setImmediate(cb)
+    }
+  }
+  var db = encdown(_db)
+  db.open(function (err) {
+    t.error(err, 'no error')
+    db.close(function (err) {
+      t.error(err, 'no error')
+      t.end()
+    })
+  })
+})
+
+test('encodings defaults to utf8', function (t) {
+  var db = encdown(memdown())
+  t.ok(db.db, '.db should be set')
+  t.ok(db.codec, '.codec should be set')
+  t.deepEqual(db.codec.opts, {
+    keyEncoding: 'utf8',
+    valueEncoding: 'utf8'
+  }, 'correct defaults')
+  t.end()
+})
+
+test('test safe decode in get', function (t) {
+  var memdb = memdown()
+  var db = encdown(memdb, { valueEncoding: 'utf8' })
+  db.put('foo', 'this {} is [] not : json', function (err) {
+    t.error(err, 'no error')
+    var db2 = encdown(memdb, { valueEncoding: 'json' })
+    db2.get('foo', function (err, value) {
+      t.equals('EncodingError', err.name)
+      memdb.close(t.end.bind(t))
+    })
+  })
+})
+
+test('can decode from string to json', function (t) {
+  var memdb = memdown()
+  var db = encdown(memdb, { valueEncoding: 'utf8' })
+  var data = { thisis: 'json' }
+  db.put('foo', JSON.stringify(data), function (err) {
+    t.error(err, 'no error')
+    var db2 = encdown(memdb, { valueEncoding: 'json' })
+    db2.get('foo', function (err, value) {
+      t.error(err, 'no error')
+      t.deepEqual(value, data, 'JSON.parse works')
+      memdb.close(t.end.bind(t))
+    })
+  })
+})
+
+test('can decode from json to string', function (t) {
+  var memdb = memdown()
+  var db = encdown(memdb, { valueEncoding: 'json' })
+  var data = { thisis: 'json' }
+  db.put('foo', data, function (err) {
+    t.error(err, 'no error')
+    var db2 = encdown(memdb, { valueEncoding: 'utf8' })
+    db2.get('foo', function (err, value) {
+      t.error(err, 'no error')
+      t.deepEqual(value, JSON.stringify(data), 'JSON.parse works')
+      memdb.close(t.end.bind(t))
+    })
+  })
+})
+
+test('binary encoding, using batch', function (t) {
+  var data = [
+    {
+      type: 'put',
+      key: Buffer.from([1, 2, 3]),
+      value: Buffer.from([4, 5, 6])
+    },
+    {
+      type: 'put',
+      key: Buffer.from([7, 8, 9]),
+      value: Buffer.from([10, 11, 12])
+    }
+  ]
+  var db = encdown(memdown(), {
+    keyEncoding: 'binary',
+    valueEncoding: 'binary'
+  })
+  db.batch(data, function (err) {
+    t.error(err, 'no error')
+    db.get(data[0].key, function (err, value) {
+      t.error(err, 'no error')
+      t.deepEqual(value, data[0].value)
+      db.get(data[1].key, function (err, value) {
+        t.error(err, 'no error')
+        t.deepEqual(value, data[1].value)
+        db.close(t.end.bind(t))
+      })
+    })
+  })
+})
